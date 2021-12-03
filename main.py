0dd91ca3cf4ecf62cb4a57993f4b77d72c06c89b
Tomas Stary <tomichec@gmail.com>
2021-12-03 07:14:02 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import requests
+from datetime import datetime
+
+def averageTime(events, eventType = 'PullRequestEvent'):
+    '''return the average time between specific events'''
+    eventCreation = []
+    for event in events:
+        print(event['type'])
+        if event['type'] == eventType:
+             eventCreation.append(
+                 datetime.strptime(event['created_at'],'%Y-%m-%dT%H:%M:%SZ').timestamp()
+             )
+    
+    # compute the average time -- this assumes that the events are sorted by time in reverse order
+    N = len(eventCreation) - 1
+    if N < 1:
+        print("Not enough {} events were present.".format(eventType))
+        return -1
+    
+    averageTime = (eventCreation[0] - eventCreation[-1])/N
+
+    return averageTime
+
+def totalEvents(events, offset = 24*60):
+    '''return a dictionary of total number of events grouped by event type for a given offset'''
+    E = {}
+    for event in events:
+        eventTimestamp = datetime.strptime(event['created_at'],'%Y-%m-%dT%H:%M:%SZ').timestamp()
+        if datetime.now().timestamp() -  eventTimestamp > offset*60:
+            break
+        try:
+            E[event['type']] += 1
+        except:
+            E[event['type']] = 1
+
+    return E
+    
+if __name__ =='__main__':
+    user = 'pandas-dev'
+    repo = 'pandas'
+
+    url = "https://api.github.com/repos/{}/{}/events".format(user,repo)
+    data = requests.get(url)
+    
+    if data.status_code != 200:
+        print("Fetching the data failed.")
+        exit()
+    
+    events = data.json()
+
+    print("average time between PullRequestEvents is:", averageTime(events))
+
+    countEventsByGroup = totalEvents(events, 24*60)
+    print("in the last 24 hours there were the following events:")
+    print("event type\tnumber of")
+    for k in countEventsByGroup.keys():
+        print("{}:  {}".format(k,countEventsByGroup[k]))
